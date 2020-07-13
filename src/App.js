a8e19d1312ad5a20d56eafad3acf3b2fec62c26c
sami-aloosi <kingxsam@gmail.com>
2020-07-12 20:24:17 -0700

    axios work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
+function Image(props) {
+  return <img src={props.url} />;
+}
+
 function App() {
+  const [images, setImages] = useState("");
+
+  axios
+    .get(
+      `https://api.nasa.gov/planetary/apod?api_key=HiRCIgjcULXuND5KQc15tzbGnzDrWmqstWWDa5QA&date=2020-07-12`
+    )
+    .then((response) => setImages(response.data.url))
+    .catch((err) => console.log("there was an error:", err));
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      </p> */}
+      <Image url={images} />
     </div>
   );
 }
